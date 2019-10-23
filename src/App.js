5cb53b6681ab4a35a7af84f65d09ba25a4e58fa2
armenio-s <mauricioarmenio@gmail.com>
2019-10-22 23:09:26 -0300

    pegada a la API

--- src/App.js
+++ src/App.js
@@ -16,10 +16,26 @@ class App extends React.Component {
           dateTo: new Date(today.valueOf() + 86400000),
           country: undefined, 
           price: undefined,
-          rooms: undefined
+          rooms: undefined,
+          hotels: []
       }
     };
   }
+
+   async componentDidMount() {
+   try {
+     const response = await fetch(
+       "https://wt-8a099f3e7c73b2d17f4e018b6cfd6131-0.sandbox.auth0-extend.com/acamica"
+     );
+     if (!response.ok) {
+       throw Error(response.statusText);
+     }
+     const json = await response.json();
+     console.log({ hotels: json, hotelsLoaded: true });
+   } catch (err) {
+     console.log(err);
+   }
+ }
 
   render() {
     return (
