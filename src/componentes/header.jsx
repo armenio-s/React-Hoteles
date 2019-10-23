cfd507c330652bf797e9b7cf38946079286ad8d6
armenio-s <mauricioarmenio@gmail.com>
2019-10-22 22:48:06 -0300

    cambios en el header

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -11,10 +11,12 @@ class Header extends React.Component {
                         <h2 className="subtitle">
                             desde el <strong>{this.props.filters.dateFrom.toLocaleString('es-AR',{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })} </strong> 
                             hasta el <strong>{this.props.filters.dateTo.toLocaleString('es-AR',{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</strong>
+                        </h2>
+                        <p>
                             { this.props.filters.country ? ` en ${this.props.filters.country}` : '' }
                             { this.props.filters.price ? ` por ${'$'.repeat(this.props.filters.price)}` : '' }
                             { this.props.filters.rooms ? ` de hasta ${this.props.filters.rooms} habitaciones` : '' }
-                        </h2>
+                        </p>
                     </div>
                 </div>
             </section>
